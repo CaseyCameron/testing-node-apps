b5c6cb21e5f4dcf8b39b8b2127af0defe4adac84
caseycameron <mcasey.cameron@gmail.com>
2022-07-20 17:14:18 -0700

    auth.exercise pure function abstraction

--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -1,15 +1,24 @@
 import {isPasswordAllowed} from '../auth'
 
-test('isPasswordAllowed returns true for valid passwords', () => {
-  expect(isPasswordAllowed('!aBc12x')).toBe(true)
-})
-
-
-test('isPasswordAllowed returns false for invalid passwords', () => {
-  expect(isPasswordAllowed('a2c!')).toBe(false)
-  expect(isPasswordAllowed('123456!')).toBe(false)
-  expect(isPasswordAllowed('ABCdef!')).toBe(false)
-  expect(isPasswordAllowed('abc123!')).toBe(false)
-  expect(isPasswordAllowed('ABCdef123')).toBe(false)
-  expect(isPasswordAllowed('ABC123!')).toBe(false)
+describe('isPasswordAllowed only allows some passwords', () => {
+  const allowedPasswords = ['!aBc12x']
+  const disallowedPasswords = [
+    'a2c!',
+    '123456!',
+    'ABCdef!',
+    'abc123!',
+    'ABCdef123',
+    'ABC123!',
+  ]
+  allowedPasswords.forEach(password => {
+    test(`allows ${password}`, () => {
+      expect(isPasswordAllowed(password)).toBe(true)
+    })
+  })
+  
+  disallowedPasswords.forEach(password => {
+    test(`disallows ${password}`, () => {
+      expect(isPasswordAllowed(password)).toBe(false)
+    })
+  })
 })
