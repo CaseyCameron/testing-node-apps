2344b9f24bb20bd5e0edb16c345c42fd5b67ec02
caseycameron <mcasey.cameron@gmail.com>
2022-07-21 11:40:32 -0700

    error-middelware refactor

--- src/utils/__tests__/error-middleware.exercise.js
+++ src/utils/__tests__/error-middleware.exercise.js
@@ -1,21 +1,13 @@
+import { buildRes, buildReq, buildNext } from 'utils/generate';
 import {UnauthorizedError} from 'express-jwt'
 import errorMiddleware from '../error-middleware'
 
-function buildRes(overrides) {
-  const res = {
-    json: jest.fn(() => res), 
-    status: jest.fn(() => res),
-    ...overrides,
-  }
-  return res
-}
-
 // 🐨 Write a test for the UnauthorizedError case
 test('responds with 401 for express-jwt Unauthorized Error', () => {
-  const req = {}
+  const req = buildReq()
   // create a normal response object just as we have it set up in buildRes
   const res = buildRes()
-  const next = jest.fn()
+  const next = buildNext()
   const code = 'some_error_code'
   const message = 'Some message'
   const error = new UnauthorizedError(code, {message})
@@ -29,8 +21,8 @@ test('responds with 401 for express-jwt Unauthorized Error', () => {
 
 // 🐨 Write a test for the headersSent case
 test('calls next if headersSent is true', () => {
-  const req = {}
-  const next = jest.fn()
+  const req = buildReq()
+  const next = buildNext()
   // build a res, this time with a headersSent property
   const res = buildRes({headersSent: true})
   const error = new Error('error')
@@ -43,8 +35,8 @@ test('calls next if headersSent is true', () => {
 
 // 🐨 Write a test for the else case (responds with a 500)
 test('responds with 500 and the error object', () => {
-  const req = {}
-  const next = jest.fn()
+  const req = buildReq()
+  const next = buildNext()
   const res = buildRes()
   const error = new Error('error')
   errorMiddleware(error, req, res, next)
